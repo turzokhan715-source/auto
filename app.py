919d6b1e5ef12cb544311bd2b051e85b648878a6
turzokhan715-source <turzokhan715@gmail.com>
2026-07-14 23:37:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = 'secret_key_123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 class User(UserMixin, db.Model):
@@ -24,6 +26,7 @@ def signup():
         email = request.form.get('email')
         password = request.form.get('password')
         confirm = request.form.get('confirm_password')
+        
         if password == confirm:
             new_user = User(email=email, password=password)
             db.session.add(new_user)
@@ -34,24 +37,26 @@ def signup():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = User.query.filter_by(email=request.form.get('email')).first()
-        if user and user.password == request.form.get('password'):
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        
+        if user and user.password == password:
             login_user(user)
             return redirect(url_for('dashboard'))
     return render_template('login.html')
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
 
 @app.route('/')
 @login_required
 def dashboard():
     return render_template('dashboard.html')
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
