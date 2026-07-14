74ac3b41e39b6c1c4f45b0f6de20c4fee57f1515
turzokhan715-source <turzokhan715@gmail.com>
2026-07-14 22:18:59 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        new_user = User(email=request.form.get('email'), password=request.form.get('password'))
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(email=request.form.get('email')).first()
+        if user and user.password == request.form.get('password'):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+    return render_template('login.html')
 
 @app.route('/')
-def home():
+@login_required
+def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
